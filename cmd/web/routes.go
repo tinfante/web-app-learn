7ad5bec9af531246b36d1a284771db85624c0b8a
Tomás Infante <toinfanteb@falabella.cl>
2023-07-20 14:37:10 -0400

    adds routing with Chi library, and moves routing to /cmd/web/routes.go

---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"web-app-learn/pkg/config"
+	"web-app-learn/pkg/handlers"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module web-app-learn
 
 go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
