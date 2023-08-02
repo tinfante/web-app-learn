ab749445073c4bc9e6b1a1d4d3aee071dfb1b84c
Tomás Infante <toinfanteb@falabella.cl>
2023-08-02 11:55:58 -0400

    adds server side validation to make reservation form.

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/tinfante/web-app-learn
 go 1.20
 
 require (
-	github.com/alexedwards/scs/v2 v2.5.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.10 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.1
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/justinas/nosurf v1.1.1
 )
+
+require github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/tinfante/web-app-learn/internal/config"
+	"github.com/tinfante/web-app-learn/internal/forms"
 	"github.com/tinfante/web-app-learn/internal/models"
 	"github.com/tinfante/web-app-learn/internal/render"
 )
@@ -56,7 +57,42 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	var emptyReservation models.Reservation
+	data := make(map[string]interface{})
+	data["reservation"] = emptyReservation
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+		Data: data,
+	})
+}
+
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	reservation := models.Reservation{
+		FirstName: r.Form.Get("first_name"),
+		LastName:  r.Form.Get("last_name"),
+		Phone:     r.Form.Get("phone"),
+		Email:     r.Form.Get("email"),
+	}
+	form := forms.New(r.PostForm)
+
+	form.Required("first_name", "last_name", "email")
+	form.MinLength("first_name", 3, r)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		data := make(map[string]interface{})
+		data["reservation"] = reservation
+		render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+			Form: form,
+			Data: data,
+		})
+		return
+	}
 }
 
 // Generals renders the room page
