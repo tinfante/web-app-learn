50e2b2c8aaf8ca5fd8d9edaf6f79af2855b46e83
Tomás Infante <toinfanteb@falabella.cl>
2023-08-03 19:56:26 -0400

    adds tests for forms.go

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,7 +2,6 @@ package forms
 
 import (
 	"fmt"
-	"net/http"
 	"net/url"
 	"strings"
 
@@ -34,16 +33,13 @@ func (f *Form) Required(fields ...string) {
 	}
 }
 
-func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
-	if x == "" {
-		return false
-	}
-	return true
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
+	return x != ""
 }
 
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long.", length))
 		return false
@@ -51,8 +47,10 @@ func (f *Form) MinLength(field string, length int, r *http.Request) bool {
 	return true
 }
 
-func (f *Form) IsEmail(field string) {
+func (f *Form) IsEmail(field string) bool {
 	if !govalidator.IsEmail(f.Get(field)) {
 		f.Errors.Add(field, "Invalid email address.")
+		return false
 	}
+	return true
 }
