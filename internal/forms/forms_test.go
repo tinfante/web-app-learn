50c1709a0438783c116c33805e20ea8015a892f5
Tomás Infante <toinfanteb@falabella.cl>
2023-08-03 21:05:41 -0400

    refactors forms_test.go. removes unecessary http requests mocking.

--- internal/forms/forms_test.go
+++ internal/forms/forms_test.go
@@ -1,15 +1,14 @@
 package forms
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"net/url"
 	"testing"
 )
 
 func TestForm_Valid(t *testing.T) {
-	r := httptest.NewRequest("POST", "/whatever", nil)
-	form := New(r.PostForm)
+	// r := httptest.NewRequest("POST", "/whatever", nil)
+	postedData := url.Values{}
+	form := New(postedData)
 
 	isValid := form.Valid()
 	if !isValid {
@@ -18,23 +17,20 @@ func TestForm_Valid(t *testing.T) {
 }
 
 func TestForm_Required(t *testing.T) {
-	r := httptest.NewRequest("POST", "/whatever", nil)
-	form := New(r.PostForm)
+	postedData := url.Values{}
+	form := New(postedData)
 
 	form.Required("a", "b", "c")
 	if form.Valid() {
 		t.Error("form shows valid when required fields missing")
 	}
 
-	postedData := url.Values{}
+	postedData = url.Values{}
 	postedData.Add("a", "a")
 	postedData.Add("b", "a")
 	postedData.Add("c", "a")
+	form = New(postedData)
 
-	r, _ = http.NewRequest("POST", "/whatever", nil)
-
-	r.PostForm = postedData
-	form = New(r.PostForm)
 	form.Required("a", "b", "c")
 	if !form.Valid() {
 		t.Error("shows does not have required fields when it does")
